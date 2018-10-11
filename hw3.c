fc7f6e295fcafd0e5f5baaa94974ba83b7fd677c
Fernando Martinez <Fernandomart@10-8-235-60.vlan877.client.wireless.uic.edu>
2018-10-11 13:47:19 -0500

    Ch

--- hw3.c
+++ hw3.c
@@ -10,23 +10,20 @@
 int main() {
   char line[500];
   char *argsarray[20];
-  //char* path= "/bin/";
-  //int j=0;
-  char fullpath[20];
+  char *temp[20];
   int i;
   int size;
 
-/*
-  for(i = 0; i < 20; i++)
-  {
-    argsarray[i] = (char* )malloc(100 * sizeof(char));
-  }
-*/
     while(1)
     {
       for(i = 0; i < 20; i++)
       {
         argsarray[i] = (char* )malloc(100 * sizeof(char));
+      }
+
+      for(i = 0; i < 20; i++)
+      {
+        temp[i] = (char* )malloc(100 * sizeof(char));
       }
         printf("CS361 >");
         fgets(line,500,stdin);
@@ -65,12 +62,54 @@ int main() {
         for(i=0;i<size;i++)
         {
           printf("arg[%d]: %s\n",i,argsarray[i]);
+          int z=0;
+
+          if(strcmp(argsarray[i],";") == 0)
+          {
+            printf("Detected\n");
+
+            for(int j=0;j<i;j++)
+            {
+              strcpy(temp[j],argsarray[j]);
+              printf("TEMP HAS: %s\n", temp[j]);
+              z++;
+            }
+            temp[z]=NULL;
+
+            i=i+1;
+            z=0;
+            for(int j=0;i<size;i++)
+            {
+              argsarray[j]=argsarray[i];
+              printf("Argsarray has: %s\n",argsarray[j]);
+              z++;
+            }
+
+            argsarray[z]=NULL;
+
+            int pid = fork();
+            if (pid == 0)//Child
+            {
+              execvp(temp[0], temp);
+              printf("Couldn't do command in temp\n");
+            }
+
+            //This is the parent
+            else
+            {
+              printf("pid:%d ",pid);
+              int status;
+              wait(&status);
+              printf("status:%d\n",WEXITSTATUS(status));
+            }
+            //break;
+          }
         }
+
 
         int pid = fork();
         if (pid == 0)//Child
         {
-          int childpid=getpid();
           execvp(argsarray[0], argsarray);
           printf("Couldn't do command\n");
         }
@@ -78,10 +117,6 @@ int main() {
         //This is the parent
         else
         {
-          if(childpid!=pid)
-          {
-            printf("Pid is different");
-          }
           printf("pid:%d ",pid);
           int status;
           wait(&status);
